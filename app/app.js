3c8d9da1d32f568cf6fce798e86d17613bdb6da4
aostrovsky <anatolyostrovsky@gmail.com>
2016-08-11 12:28:53 +0300

    Refactor to ES6

--- app/app.js
+++ app/app.js
@@ -1,9 +1,24 @@
 import angular from 'angular';
 import 'angular-route';
 import 'angular-ui-router';
-import fastclick from 'fastclick';
+import fs from 'fastclick';
 import 'base-apps';
 import 'base-apps-templates';
+
+const AppConfig = ($urlProvider, $locationProvider) => {
+  $urlProvider.otherwise('/');
+
+  $locationProvider.html5Mode({
+    enabled:false,
+    requireBase: false
+  });
+};
+
+AppConfig.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+const AppRun = () => {
+  fs.FastClick.attach(document.body);
+};
 
 angular.module('application', [
   'ui.router',
@@ -12,22 +27,5 @@ angular.module('application', [
   'base.dynamicRouting',
   'base.dynamicRouting.animations'
 ])
-.config(config)
-.run(run);
-
-config.$inject = ['$urlRouterProvider', '$locationProvider'];
-
-function config($urlProvider, $locationProvider) {
-  $urlProvider.otherwise('/');
-
-  $locationProvider.html5Mode({
-    enabled:false,
-    requireBase: false
-  });
-
-  $locationProvider.hashPrefix('!');
-}
-
-function run() {
-  fastclick.FastClick.attach(document.body);
-}
+.config(AppConfig)
+.run(AppRun);
