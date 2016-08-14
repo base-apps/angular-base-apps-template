baa0e6d1a241f04a324d048f50761ee69db2304a
Brian Soumakian <bsoumakian@gmail.com>
2016-08-14 14:29:28 -0700

    update to angular-base-apps 2.0
    https://github.com/base-apps/angular-base-apps/issues/32

--- app/app.js
+++ app/app.js
@@ -2,8 +2,10 @@ import angular from 'angular';
 import 'angular-route';
 import 'angular-ui-router';
 import fs from 'fastclick';
-import 'base-apps';
-import 'base-apps-templates';
+import 'angular-base-apps/dist/js/base-apps';
+import 'angular-base-apps/dist/js/base-apps-templates';
+import 'angular-dynamic-routing/dynamicRouting';
+import 'angular-dynamic-routing/dynamicRouting.animations';
 
 const AppConfig = ($urlProvider, $locationProvider) => {
   $urlProvider.otherwise('/');
@@ -22,10 +24,13 @@ const AppRun = () => {
 
 angular.module('application', [
   'ui.router',
-  //base
+
+  // base apps
   'base',
-  'base.dynamicRouting',
-  'base.dynamicRouting.animations'
+
+  // dynamic routing
+  'dynamicRouting',
+  'dynamicRouting.animations'
 ])
 .config(AppConfig)
 .run(AppRun);
