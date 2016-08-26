824bb30a7d3bef0cdbb5965af8d1bb4d1bf25d6f
Brian Soumakian <bsoumakian@gmail.com>
2016-08-25 22:46:23 -0700

    allow running chrome on travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
   'use strict';
 
   module.exports = function (config) {
-    config.set({
+    var configuration = {
 
       // base path that will be used to resolve all patterns (eg. files, exclude)
       basePath: '',
@@ -62,11 +62,22 @@
       // start these browsers
       // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
       browsers: ['Chrome'],
-
+      customLaunchers: {
+        Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: ['--no-sandbox']
+        }
+      },
 
       // Continuous Integration mode
       // if true, Karma captures browsers, runs the tests and exits
       singleRun: false
     });
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
   };
 }());
