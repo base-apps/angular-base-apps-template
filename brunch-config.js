dce1642b0a106c6539ed993b0a11cd6b43c90fc0
aostrovsky <anatolyostrovsky@gmail.com>
2016-09-16 13:05:58 +0300

    Update configuration

--- brunch-config.js
+++ brunch-config.js
@@ -3,6 +3,17 @@ const router = require('base-apps-router');
 const mkdirp = require('mkdirp');
 
 module.exports = {
+
+  conventions: {
+    assets:   /^(app)(\\|\/)(assets)/,
+    ignored:  [/\/_/, /\.(spec|scenario)\.(js$)/]
+  },
+
+  paths: {
+    'public':   'build',
+    'watched':  ['app']
+  },
+
   files: {
     javascripts: {
       joinTo: {
@@ -21,15 +32,13 @@ module.exports = {
     },
     eslint: {
       // do not fail build when running against config-routes.js
-      pattern: /^app\/config\/config-routes\.js$/,
+      pattern: /^config-routes\.js$/,
       warnOnly: true
     },
     htmlPages: {
       forceRemoveFrontMatter: true
     }
   },
-
-  optimize: false,
 
   hooks: {
     preCompile: (done) => {
@@ -44,24 +53,7 @@ module.exports = {
         library: 'node',
         overwrite: true
       }).then(done);
-    },
-
-    // TODO: Remove once htmlPages supports forceRemoveFrontMatter option
-    onCompile: function () {
-      router({
-        src: './public/**/*.html',
-        dest: './public',
-        path: './build/routes.js',
-        root: './public',
-        library: 'node',
-        overwrite: true
-      });
-    }
-  },
-
-  overrides: {
-    production: {
-      optimize: true
     }
   }
+
 };
